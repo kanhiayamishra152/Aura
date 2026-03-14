7e167ce081ff8a99eb4d97514d94237877a6c71d
@Praxai <kanhiayamishra152@gmail.com>
2026-03-14 11:41:02 +0530

    Create premium_text_field.dart

---
+++ lib/ui/widgets/premium_text_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../../config/app_colors.dart';
+
+class PremiumTextField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final IconData? prefixIcon;
+
+  const PremiumTextField({
+    Key? key,
+    required this.hintText,
+    required this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.prefixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.grey[600]),
+        filled: true,
+        fillColor: AppColors.surfaceBlack,
+        prefixIcon: prefixIcon != null ? Icon(prefixIcon, color: Colors.grey) : null,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: AppColors.accentGreen, width: 1),
+        ),
+      ),
+    );
+  }
+}
